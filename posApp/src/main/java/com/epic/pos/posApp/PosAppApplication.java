79c9d72b7646024904ed1f905dca0ef60b79349f
jpssasadara <jpssasadara1995@gmail.com>
2020-01-07 14:13:22 +0530

    Start New project

---
+++ posApp/src/main/java/com/epic/pos/posApp/PosAppApplication.java
@@ -0,0 +1,13 @@
+package com.epic.pos.posApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosAppApplication.class, args);
+	}
+
+}
