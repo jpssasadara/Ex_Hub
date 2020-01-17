1e91d2d0e312206b792e0fa91339e0d7d72b35bc
jpssasadara <jpssasadara1995@gmail.com>
2020-01-17 12:06:05 +0530

    interceptors were added

---
+++ posApp/src/main/java/com/epic/pos/posApp/Interceptor/WebMVCConfig.java
@@ -0,0 +1,29 @@
+package com.epic.pos.posApp.Interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by oa on 6/3/2019.
+ */
+@Configuration
+public class WebMVCConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new LogInterceptor())
+                .addPathPatterns("/login/GetUserbyUsername/*")
+                .addPathPatterns("/Testing")
+                .excludePathPatterns("/secure-code/public"); //by default applies to
+        registry.addInterceptor(new AuthInterceptor()).addPathPatterns("/auth");
+
+        // Register admin interceptor with multiple path patterns
+        /*registry.addInterceptor(new LogInterceptor())
+                .addPathPatterns(new String[] { "/admin", "/admin*//*" });
+*/
+//        registry.addInterceptor((new SubLogInterceptor())).addPathPatterns("/admin/*").excludePathPatterns("admin/oldLogin");
+
+
+    }
+}
