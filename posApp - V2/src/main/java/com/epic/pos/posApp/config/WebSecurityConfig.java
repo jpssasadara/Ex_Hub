b246199d3d51e37cb67f9935a4aacc78a9f63f4f
jpssasadara <jpssasadara1995@gmail.com>
2020-01-27 14:17:41 +0530

    Fix last week cross issure by doing some modifications in spring security config file

--- posApp - V2/src/main/java/com/epic/pos/posApp/config/WebSecurityConfig.java
+++ posApp - V2/src/main/java/com/epic/pos/posApp/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.epic.pos.posApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -52,13 +53,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
-				// dont authenticate this particular request
-				.authorizeRequests().antMatchers( "/register/addNewUser","/login/authenticate").permitAll().
+				// dont authenticate this particular request                                                                        //allow CORS option calls
+				.authorizeRequests().antMatchers( "/register/addNewUser","/login/authenticate").permitAll().antMatchers(HttpMethod.OPTIONS, "/**")
+				.permitAll().
 				// all other requests need to be authenticated
-				anyRequest().authenticated().and().
+						anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
 				// store user's state.
-				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+						exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 		// Add a filter to validate the tokens with every request
